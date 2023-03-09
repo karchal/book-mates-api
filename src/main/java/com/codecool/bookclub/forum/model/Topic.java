cebfaae79703247152abd7eb7e3493b16c3994e8
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-09 11:49:48 +0100

    create empty classes in forum package

--- src/main/java/com/codecool/bookclub/forum/model/Topic.java
+++ src/main/java/com/codecool/bookclub/forum/model/Topic.java
@@ -1,4 +1,14 @@
 package com.codecool.bookclub.forum.model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Topic {
+
+    private int id;
+    private String title;
+    private String msg;
+    private LocalDateTime creationTime;
+    private List<Comment> comments = new ArrayList<>();
 }
