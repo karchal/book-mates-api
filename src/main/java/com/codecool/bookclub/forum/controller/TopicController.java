eba57f844a2781d53794c868c7314b46f2bc53b9
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-09 10:38:58 +0100

    add "/api" to endpoint and change name of service

--- src/main/java/com/codecool/bookclub/forum/controller/TopicController.java
+++ src/main/java/com/codecool/bookclub/forum/controller/TopicController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@RestController("/api")
 public class TopicController {
 
     @GetMapping("/books/{book_id}/topic")
