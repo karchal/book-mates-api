9489f95e42e9c8ccce96b72faa5b15e1c6062a1c
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-09 10:46:05 +0100

    create crud frame in CommentController

--- src/main/java/com/codecool/bookclub/forum/controller/PostController.java
+++ src/main/java/com/codecool/bookclub/forum/controller/PostController.java
@@ -1,8 +1,37 @@
 package com.codecool.bookclub.forum.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.bookclub.forum.model.Comment;
+import com.codecool.bookclub.forum.model.Topic;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController("/api")
 public class PostController {
+
+
+    @GetMapping("/books/{book_id}/topics/{topic_id}")
+    public List<Topic> getCommentsToTopic(@PathVariable("book_id") long bookId, @PathVariable("topic_id") long topicId){
+        return new ArrayList<>();
+    }
+
+
+    @PostMapping("/books/{book_id}/topics/{topic_id}")
+    public boolean createComment(@PathVariable("book_id") long bookId, @PathVariable("topic_id") long topicId, @RequestBody Comment comment){
+        return false;
+    }
+
+
+    @PutMapping("/books/{book_id}/topics/{topic_id}")
+    public boolean updateComment(@PathVariable("book_id") long bookId, @PathVariable("topic_id") long topicId, @RequestBody Comment comment) {
+        return false;
+    }
+
+    @DeleteMapping("/books/{book_id}/topics/{topic_id}")
+    public boolean deleteComment(@PathVariable("book_id") long bookId, @PathVariable("topic_id") long topicId, @RequestBody Comment comment){
+        return false;
+    }
+
 
 }
