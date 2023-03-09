90989a324378d9a9562c044c51c5680a49db4cf6
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-09 10:17:50 +0100

    rename methods in TopicController

--- src/main/java/com/codecool/bookclub/forum/controller/TopicController.java
+++ src/main/java/com/codecool/bookclub/forum/controller/TopicController.java
@@ -10,22 +10,22 @@ import java.util.List;
 public class TopicController {
 
     @GetMapping("/books/{book_id}/topic")
-    public List<Topic> getThemesForBook(@PathVariable("book_id") long id){
+    public List<Topic> getTopicsForBook(@PathVariable("book_id") long id){
         return new ArrayList<>();
     }
 
     @PostMapping("/books/{book_id}/topic")
-    public boolean createTheme(@PathVariable("book_id") long id, @RequestBody Topic topic){
+    public boolean createTopic(@PathVariable("book_id") long id, @RequestBody Topic topic){
         return false;
     }
 
     @PutMapping("/books/{book_id}/topic")
-    public boolean updateTheme(@PathVariable("book_id") long id, @RequestBody Topic updatedTopic) {
+    public boolean updateTopic(@PathVariable("book_id") long id, @RequestBody Topic updatedTopic) {
         return false;
     }
 
     @DeleteMapping("/books/{book_id}/topic")
-    public boolean deleteTheme(@PathVariable("book_id") long id, @RequestBody Topic topic){
+    public boolean deleteTopic(@PathVariable("book_id") long id, @RequestBody Topic topic){
         return false;
     }
 
