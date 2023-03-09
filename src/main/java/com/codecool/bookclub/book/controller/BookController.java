75ef253edcb04ce5851d11678145e723e602314e
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-09 15:11:23 +0100

    add RequestMapping before controller

--- src/main/java/com/codecool/bookclub/book/controller/BookController.java
+++ src/main/java/com/codecool/bookclub/book/controller/BookController.java
@@ -2,40 +2,26 @@ package com.codecool.bookclub.book.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/api")
 @RestController
 public class BookController {
 
-    @GetMapping("api/books")
+    @GetMapping("/books")
     public String getAll(@RequestParam(value = "author", required = false) String author,
                          @RequestParam(value = "title", required = false) String title,
                          @RequestParam(value = "genre", required = false) String genre) {
         return "Here you'll see all books";
     }
 
-    @GetMapping("api/books/{id}")
+    @GetMapping("/books/{id}")
     public String getBookById(@PathVariable("id") String id) {
         return "The book details";
     }
 
-    @PostMapping("api/books/{id}/rate")
+    @PostMapping("books/{id}/rate")
     public String rate(@PathVariable("id") String id, @RequestBody int rating) {
         return "Rate a book";
     }
-
-    /**
-     * Returns the shelves. By default, only logged user shelves are returned, unless
-     * userId parameter is provided.
-     * @param userId If provided, the specific user's shelves are returned
-     * @return
-     */
-    @GetMapping("")
-    public String getShelves(@RequestParam(value = "userId", required = false) String userId) {
-        return "";
-
-    }
-
-
-
 
 
 
