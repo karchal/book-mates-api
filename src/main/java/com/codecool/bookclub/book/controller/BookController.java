3174bf79b3ebe65cf0520115ba372e7c97c17c66
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2023-03-09 14:56:58 +0100

    move api from bean annotation to method annotation

--- src/main/java/com/codecool/bookclub/book/controller/BookController.java
+++ src/main/java/com/codecool/bookclub/book/controller/BookController.java
@@ -2,22 +2,22 @@ package com.codecool.bookclub.book.controller;
 
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/api")
+@RestController
 public class BookController {
 
-    @GetMapping("/books")
+    @GetMapping("api/books")
     public String getAll(@RequestParam(value = "author", required = false) String author,
                          @RequestParam(value = "title", required = false) String title,
                          @RequestParam(value = "genre", required = false) String genre) {
         return "Here you'll see all books";
     }
 
-    @GetMapping("/books/{id}")
+    @GetMapping("api/books/{id}")
     public String getBookById(@PathVariable("id") String id) {
         return "The book details";
     }
 
-    @PostMapping("books/{id}/rate")
+    @PostMapping("api/books/{id}/rate")
     public String rate(@PathVariable("id") String id, @RequestBody int rating) {
         return "Rate a book";
     }
