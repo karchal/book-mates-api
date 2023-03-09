861cbc21329153810b0c9ebd8acb287d2642fca3
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-09 10:53:16 +0100

    rename controller

--- src/main/java/com/codecool/bookclub/forum/controller/CommentController.java
+++ src/main/java/com/codecool/bookclub/forum/controller/CommentController.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController("/api")
-public class PostController {
+public class CommentController {
 
 
     @GetMapping("/books/{book_id}/topics/{topic_id}")
