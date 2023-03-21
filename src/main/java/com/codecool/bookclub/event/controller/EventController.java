9b428588ef3ff53016868c098897f325b2842f07
Aneta Wawer <anwawer1@gmail.com>
2023-03-21 13:15:08 +0100

    implemented eventService to all methods

--- src/main/java/com/codecool/bookclub/event/controller/EventController.java
+++ src/main/java/com/codecool/bookclub/event/controller/EventController.java
@@ -1,36 +1,61 @@
 package com.codecool.bookclub.event.controller;
 
+import com.codecool.bookclub.event.model.Event;
 import com.codecool.bookclub.event.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.w3c.dom.events.Event;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class EventController {
 
 
-    private EventService eventService;
+    private final EventService eventService;
 
-    @GetMapping("/books/{book_id}/events")
-    public List<Event> getEventsForBook(@PathVariable("book_id") long id){return new ArrayList<>();}
-
-
-    @PostMapping("/books/{book_id}/new-event")
-    public boolean createEvent(@PathVariable("book_id") long id, @RequestBody Event event){
-        return false;
+    @Autowired
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
     }
 
+//    @GetMapping("/books/{book_id}/events")
+//    public List<Event> getEventsForBook(@PathVariable("book_id") long bookId){
+//        return eventService.getEventById(id);
+//        return new ArrayList<>();
+//    }
+
+    @GetMapping("/events")
+    public List<Event> getALlEvents(){
+        return eventService.getAllEvents();
+    }
+
+    @GetMapping("events/{event_id}")
+    public Optional<Event> getEventById(@PathVariable("event_id") long eventId){
+        return eventService.getEventById(eventId);
+    }
+
+    @PostMapping("/events")
+    public void addEvent(@RequestBody Event event){
+        eventService.addEvent(event.getTitle(), event.getDescription(), event.getEventDate(), event.getMaxParticipants(), event.getUrl());
+    }
+
+
+//    @PostMapping("/books/{book_id}/new-event")
+//    public boolean createEvent(@PathVariable("book_id") long bookId, @RequestBody Event event){
+//        return false;
+//    }
+
     @PutMapping("events/update-event/{event_id}")
-    public boolean updateEvent (@PathVariable("event_id") long id, @RequestBody Event updatedEvent) {
-        return false;
+    public void updateEventById (@PathVariable("event_id") long eventId, @RequestBody Event event) {
+        eventService.updateEventById(event.getId(),event.getTitle(), event.getDescription(), event.getEventDate(), event.getMaxParticipants(), event.getUrl());
     }
 
     @DeleteMapping("events/delete-event/{event_id}")
-    public boolean deleteEvent (@PathVariable("event_id") long id, @RequestBody Event event){
-        return false;
+    public boolean deleteEvent (@PathVariable("event_id") long eventId){
+        return eventService.deleteEventById(eventId);
     }
 
 
