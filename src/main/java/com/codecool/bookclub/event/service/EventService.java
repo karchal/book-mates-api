d7a2397f6ea924e4954f3a429f26595236dc8c3e
Aneta Wawer <anwawer1@gmail.com>
2023-03-21 13:14:44 +0100

    implemented eventRepository to all methods

--- src/main/java/com/codecool/bookclub/event/service/EventService.java
+++ src/main/java/com/codecool/bookclub/event/service/EventService.java
@@ -1,14 +1,57 @@
 package com.codecool.bookclub.event.service;
 
-import com.codecool.bookclub.event.dto.EventDto;
 import com.codecool.bookclub.event.model.Event;
 import com.codecool.bookclub.event.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EventService {
 
-    private EventRepository eventRepository;
+    private final EventRepository eventRepository;
+
+    @Autowired
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public List<Event> getAllEvents(){
+        return eventRepository.findAll(Sort.by(Sort.Direction.DESC, "creationDateAndTime"));
+    }
+
+    public Optional<Event> getEventById(long eventId){
+        return eventRepository.findById(eventId);
+    }
+
+    public void addEvent(String title, String description, LocalDateTime eventDate, int maxParticipants, String url){
+        eventRepository.save(new Event(title,description,eventDate,maxParticipants,url));
+    }
+
+    public boolean deleteEventById(long eventId){
+        return getEventById(eventId).map(event -> {
+            eventRepository.deleteById(eventId);
+            return true;
+        }).orElse(false);
+    }
+
+    public void updateEventById(long eventId,String title, String description, LocalDateTime eventDate, int maxParticipants, String url){
+        if (getEventById(eventId).isPresent()){
+            Event updatedEvent = getEventById(eventId).get();
+            updatedEvent.setTitle(title);
+            updatedEvent.setDescription(description);
+            updatedEvent.setEventDate(eventDate);
+            updatedEvent.setMaxParticipants(maxParticipants);
+            updatedEvent.setUrl(url);
+            eventRepository.save(updatedEvent);
+        }
+    }
+
+
 
 //    public EventServiceImplementation(EventRepository eventRepository){this.eventRepository = eventRepository;}
 //
@@ -30,12 +73,12 @@ public class EventService {
 //       return mapToEventDto(event);
 //    }
 
-    private EventDto mapToEventDto(Event event){
-        EventDto eventDto = EventDto.builder()
-                .id(event.getId())
-                .creationDateAndTime(event.getCreationDateAndTime())
-                .title(event.getTitle())
-                .build();
-        return eventDto;
-    }
+//    private EventDto mapToEventDto(Event event){
+//        EventDto eventDto = EventDto.builder()
+//                .id(event.getId())
+//                .creationDateAndTime(event.getCreationDateAndTime())
+//                .title(event.getTitle())
+//                .build();
+//        return eventDto;
+//    }
 }
