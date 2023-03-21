9acc5f3773d34c153a022634cf1aac5e146daf10
Aneta Wawer <anwawer1@gmail.com>
2023-03-21 13:11:32 +0100

    comment jpa annotations

--- src/main/java/com/codecool/bookclub/user/model/User.java
+++ src/main/java/com/codecool/bookclub/user/model/User.java
@@ -4,20 +4,33 @@ import com.codecool.bookclub.book.model.Book;
 import com.codecool.bookclub.book.model.Shelf;
 import com.codecool.bookclub.event.model.Event;
 import com.codecool.bookclub.forum.model.Topic;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
+@Entity
+@Table(name = "readers")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
     private LocalDateTime creationDate;
-    private Map<Shelf, List<Book>> books;
-    private List<Topic> topics;
+//    @JoinTable
+//    @OneToMany
+//    private Map<Shelf, List<Book>> books;
+//    @JoinTable
+//    @OneToMany
+//    private List<Topic> topics;
+    @JoinTable
+    @OneToMany
     private List<Event> events;
-    private Map<Book, BigDecimal> booksRatings;
+//    @JoinTable
+//    @OneToMany
+//    private Map<Book, BigDecimal> booksRatings;
 
 }
