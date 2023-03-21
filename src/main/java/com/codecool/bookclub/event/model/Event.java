d7a0a67274f8c0c19a46ad662e0f7d1f59d594ce
Aneta Wawer <anwawer1@gmail.com>
2023-03-21 13:10:49 +0100

    added jpa annotations

--- src/main/java/com/codecool/bookclub/event/model/Event.java
+++ src/main/java/com/codecool/bookclub/event/model/Event.java
@@ -1,39 +1,46 @@
 package com.codecool.bookclub.event.model;
 
-
 import com.codecool.bookclub.user.model.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-//@Entity
-//@Table(name = "event")
+@Entity
+@Table(name = "events")
 public class Event {
-
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-//    @CreationTimestamp
+    @CreationTimestamp
     private LocalDateTime creationDateAndTime;
     private String title;
     private String description;
     private LocalDateTime eventDate;
+    @JoinTable
+    @OneToOne
     private User organizer;
     private Integer maxParticipants;
+    @JoinTable
+    @OneToMany
     private List<User> participants;
+    @JoinTable
+    @OneToMany
     private List<User> waitingList;
     private String url;
 
-
+    public Event(String title, String description, LocalDateTime eventDate, Integer maxParticipants, String url) {
+        this.title = title;
+        this.description = description;
+        this.eventDate = eventDate;
+        this.maxParticipants = maxParticipants;
+        this.url = url;
+    }
 }
