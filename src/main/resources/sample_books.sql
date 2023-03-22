effa4ec1ac7972702621b434fe09a3b7453e1029
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-22 14:31:59 +0100

    correct data in sql

--- src/main/resources/sample_books.sql
+++ src/main/resources/sample_books.sql
@@ -1,4 +1,4 @@
-insert into book (title, author, description, pages, rating, year) values ('Gdzie śpiewają raki', 'Delia Owens', 'Światowa sensacja', 200, 8.1, 2023);
+insert into book (title, author, description, pages, rating, year) values ('Gdzie śpiewają raki', 'Delia Owens', 'Światowa sensacja', 416, 7.9, 2023);
 insert into book (title, author, description, pages, rating, year) values ('Opowieści o pilocie Pirxie', 'Stanisław Lem', 'Jedna z najbardziej znanych i lubianych książek Lema', 515, 7.9, 2012);
 insert into book (title, author, description, pages, rating, year) values ('Wyspa Robinsona', 'Arkady Fiedler', 'Piękna powieść przygodowa dla młodzieży', 246, 6.5, 1973);
 insert into book (title, author, description, pages, rating, year) values ('451 stopni Fahrenheita', 'Ray Bradbury', 'Przerażająco prorocza powieść o przyszłości w świecie bez książek', 152, 7.6, 2018);
