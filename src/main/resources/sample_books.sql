03ac5af847706f22249809ce0baa1bd1ec50b198
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2023-03-22 14:07:45 +0100

    add more books

--- src/main/resources/sample_books.sql
+++ src/main/resources/sample_books.sql
@@ -1,2 +1,6 @@
 insert into book (title, author, description, pages, rating, year) values ('Gdzie śpiewają raki', 'Delia Owens', 'Światowa sensacja', 200, 8.1, 2023);
+insert into book (title, author, description, pages, rating, year) values ('Opowieści o pilocie Pirxie', 'Stanisław Lem', 'Jedna z najbardziej znanych i lubianych książek Lema', 515, 7.9, 2012);
+insert into book (title, author, description, pages, rating, year) values ('Wyspa Robinsona', 'Arkady Fiedler', 'Piękna powieść przygodowa dla młodzieży', 246, 6.5, 1973);
+insert into book (title, author, description, pages, rating, year) values ('451 stopni Fahrenheita', 'Ray Bradbury', 'Przerażająco prorocza powieść o przyszłości w świecie bez książek', 152, 7.6, 2018);
+insert into book (title, author, description, pages, rating, year) values ('Wiedźmin. Ostatnie życzenie', 'Andrzej Sapkowski', 'Pierwsza książka z cyklu o Wiedźminie wprowadza nas w świat stworzony przez Sapkowskiego.', 330, 8.4, 2014);
 
