f817516296d3c74e3648c857af9dd41f604ceb07
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2023-03-23 20:16:32 +0100

    add methods to search books by author, title and genre in service

--- src/main/java/com/codecool/bookclub/book/repository/BookRepository.java
+++ src/main/java/com/codecool/bookclub/book/repository/BookRepository.java
@@ -2,7 +2,18 @@ package com.codecool.bookclub.book.repository;
 
 import com.codecool.bookclub.book.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book findBookById(long id);
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByTitle(String title);
+
+    List<Book> findByGenre(String genre);
+
+    List<Book> findAll();
 }
