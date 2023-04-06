bd3294e75450caab8c922f9871bc9795fe86ed07
karchal <k.s.chalupka@gmail.com>
2023-04-06 19:06:37 +0200

    create TopicDto

--- src/main/java/com/codecool/bookclub/forum/dto/TopicDto.java
+++ src/main/java/com/codecool/bookclub/forum/dto/TopicDto.java
@@ -1,4 +1,22 @@
 package com.codecool.bookclub.forum.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TopicDto {
+    private Long id;
+    private Long bookId;
+    private String bookPictureUrl;
+    private String bookTitle;
+    private String bookAuthor;
+    private String title;
+    private String authorName;
+    private LocalDateTime creationTime;
+
 }
