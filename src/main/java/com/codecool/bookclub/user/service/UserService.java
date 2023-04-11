0a2eb5ff2e7a20b7324084f45bcc328c50c2e9b3
Aneta Wawer <anwawer1@gmail.com>
2023-04-11 11:53:44 +0200

    refactor - methods to return EventDto and TopicDto

--- src/main/java/com/codecool/bookclub/user/service/UserService.java
+++ src/main/java/com/codecool/bookclub/user/service/UserService.java
@@ -1,18 +1,19 @@
 package com.codecool.bookclub.user.service;
 
 import com.codecool.bookclub.book.dto.BookDetailsDto;
-import com.codecool.bookclub.book.model.Book;
-import com.codecool.bookclub.book.model.BookDetails;
 import com.codecool.bookclub.book.repository.BookDetailsRepository;
-import com.codecool.bookclub.event.model.Event;
+import com.codecool.bookclub.event.dto.EventDto;
+import com.codecool.bookclub.event.repository.EventRepository;
+import com.codecool.bookclub.event.service.EventService;
+import com.codecool.bookclub.forum.dto.TopicDto;
 import com.codecool.bookclub.forum.model.Comment;
-import com.codecool.bookclub.forum.model.Topic;
+import com.codecool.bookclub.forum.repository.TopicRepository;
+import com.codecool.bookclub.forum.service.TopicService;
 import com.codecool.bookclub.user.model.User;
 import com.codecool.bookclub.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -22,22 +23,34 @@ public class UserService {
     private final UserRepository userRepository;
     private final BookDetailsRepository bookDetailsRepository;
 
+    private final EventRepository eventRepository;
+
+    private final TopicRepository topicRepository;
+
+    private final EventService eventService;
+
+    private final TopicService topicService;
+
     @Autowired
-    public UserService(UserRepository userRepository, BookDetailsRepository bookDetailsRepository) {
+    public UserService(UserRepository userRepository, BookDetailsRepository bookDetailsRepository, EventRepository eventRepository, TopicRepository topicRepository, EventService eventService, TopicService topicService) {
         this.userRepository = userRepository;
         this.bookDetailsRepository = bookDetailsRepository;
+        this.eventRepository = eventRepository;
+        this.topicRepository = topicRepository;
+        this.eventService = eventService;
+        this.topicService = topicService;
     }
 
     public Optional<User> getUserById(long userId){
         return userRepository.findById(userId);
     }
 
-    public List<Event> getUserEvents(long userId){
-        return getUserById(userId).map(User::getEvents).orElse(new ArrayList<>());
+    public List<EventDto> getUserEvents(long userId){
+    return eventRepository.findAllByParticipantsId(userId).stream().map(eventService::convertToDto).collect(Collectors.toList());
     }
 
-    public List<Topic> getUserTopics(long userId){
-        return getUserById(userId).map(User::getTopics).orElse(null);
+    public List<TopicDto> getUserTopics(long userId){
+        return topicRepository.findAllByAuthorId(userId).stream().map(topicService::convertToDto).collect(Collectors.toList());
     }
 
     public List<Comment> getUserComments(long userId){
@@ -63,4 +76,5 @@ public class UserService {
                 .collect(Collectors.toList());
 
     }
+
 }
