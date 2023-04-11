d46113e38a39e80c06c395187c37bb04e13b7bfb
karchal <k.s.chalupka@gmail.com>
2023-04-11 16:29:26 +0200

    remove unnecessary sql scripts

--- src/main/resources/sample_events.sql
+++
@@ -1,6 +0,0 @@
-insert into event (creation_date_and_time, description, event_date, max_participants, title, url, book_id)
-values ('2023-03-28T12:00','Wspólne czytanie', '2023-04-5T18:00', 15, 'Wspólne czytanie','https://s.lubimyczytac.pl/upload/books/240000/240310/490965-352x500.jpg',1 );
-insert into event (creation_date_and_time, description, event_date, max_participants, title, url, book_id)
-values ('2023-03-28T12:00','Wspólne czytanie', '2023-04-5T18:00', 15, 'Wspólne czytanie','https://s.lubimyczytac.pl/upload/books/148000/148887/352x500.jpg',1 );
-INSERT INTO event (creation_date_and_time, description, event_date, max_participants, title, url, organizer_id)
-VALUES (null,'Pędząć za marzeniami, kochąjąc i tracąc, pragnąc i żałując, dochodząć gdzie indziej niż by się chciało czyli porzążając za poezją Lreopolda Staffa zapraszamy na spotkanie Dyskusyjnego Klubu Książki.',  '2023-04-5T18:00', 12, 'Spotkanie Klubu Książki', null, null)
