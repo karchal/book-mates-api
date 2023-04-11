9c2612a7bc03af09c0ac7e68d4fe5c0303bb118d
Aneta Wawer <anwawer1@gmail.com>
2023-04-11 11:51:58 +0200

    removed unused imports

--- src/main/java/com/codecool/bookclub/event/repository/EventRepository.java
+++ src/main/java/com/codecool/bookclub/event/repository/EventRepository.java
@@ -1,8 +1,5 @@
 package com.codecool.bookclub.event.repository;
 
-import com.codecool.bookclub.book.model.Book;
-import com.codecool.bookclub.book.model.BookDetails;
-import com.codecool.bookclub.event.dto.EventDto;
 import com.codecool.bookclub.event.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
