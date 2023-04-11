892beca610ef4672fb9ef65089edde2aeb37316c
Aneta Wawer <anwawer1@gmail.com>
2023-04-11 11:57:46 +0200

    refactor - methods to return EventDto and TopicDto

--- src/main/java/com/codecool/bookclub/user/controller/UserController.java
+++ src/main/java/com/codecool/bookclub/user/controller/UserController.java
@@ -3,7 +3,9 @@ package com.codecool.bookclub.user.controller;
 import com.codecool.bookclub.book.dto.BookDetailsDto;
 import com.codecool.bookclub.book.model.Book;
 import com.codecool.bookclub.book.model.BookDetails;
+import com.codecool.bookclub.event.dto.EventDto;
 import com.codecool.bookclub.event.model.Event;
+import com.codecool.bookclub.forum.dto.TopicDto;
 import com.codecool.bookclub.forum.model.Comment;
 import com.codecool.bookclub.forum.model.Topic;
 import com.codecool.bookclub.user.model.User;
@@ -35,13 +37,13 @@ public class UserController {
     }
 
     @GetMapping("/users/{user_id}/events")
-    public List<Event> getUserEvents(@PathVariable("user_id") long userId){
-        return userService.getUserEvents(userId);
+    public ResponseEntity<List<EventDto>>  getUserEvents(@PathVariable("user_id") long userId){
+        return new ResponseEntity<>(userService.getUserEvents(userId), HttpStatus.OK);
     }
 
     @GetMapping("/users/{user_id}/topics")
-    public List<Topic> getUserTopics(@PathVariable("user_id") long userId){
-        return userService.getUserTopics(userId);
+    public ResponseEntity<List<TopicDto>>  getUserTopics(@PathVariable("user_id") long userId){
+       return new ResponseEntity<>(userService.getUserTopics(userId), HttpStatus.OK);
     }
 
     @GetMapping("/users/{user_id}/comments")
