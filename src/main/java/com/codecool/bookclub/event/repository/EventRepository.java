b2b89d9f9a6433cfd3b2fd88118519a3b3bba7a8
Aneta Wawer <anwawer1@gmail.com>
2023-04-11 11:51:33 +0200

    added findAllByParticipantsId() method

--- src/main/java/com/codecool/bookclub/event/repository/EventRepository.java
+++ src/main/java/com/codecool/bookclub/event/repository/EventRepository.java
@@ -1,6 +1,8 @@
 package com.codecool.bookclub.event.repository;
 
 import com.codecool.bookclub.book.model.Book;
+import com.codecool.bookclub.book.model.BookDetails;
+import com.codecool.bookclub.event.dto.EventDto;
 import com.codecool.bookclub.event.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +13,8 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Event findEventById(long id);
 
     List<Event> findFirst4ByOrderByCreationDateAndTimeDesc();
+
+    List<Event> findAllByParticipantsId(long userId);
+
+
 }
