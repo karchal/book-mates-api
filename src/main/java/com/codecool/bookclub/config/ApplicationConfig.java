95f7602f86e6b35aebb4b62637d07a37ebd2499b
karchal <k.s.chalupka@gmail.com>
2023-04-14 15:59:52 +0200

    create new beans: AuthenticationProvider, AuthenticationManager, PasswordEncoder

--- src/main/java/com/codecool/bookclub/config/ApplicationConfig.java
+++ src/main/java/com/codecool/bookclub/config/ApplicationConfig.java
@@ -4,21 +4,46 @@ import com.codecool.bookclub.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
+    private final UserRepository userRepository;
 
-//    private final UserRepository userRepository;
-//
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        //implementing UserDetailsService it is necessary and sufficient to implement loadUserByUsername, that's why we use lambda expression
-//        return username -> userRepository
-//                .findByUsername(username)
-//                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-//    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        //to implement UserDetailsService it is necessary and sufficient to implement loadUserByUsername,
+        // but instead of returning a class with overridden method we use lambda expression
+        return username -> userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
