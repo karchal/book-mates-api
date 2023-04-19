f515c33d258f0e6349b0453e1d612824540adf70
Aneta Wawer <anwawer1@gmail.com>
2023-04-19 10:36:17 +0200

    created event details

---
+++ src/main/java/com/codecool/bookclub/event/model/EventDetails.java
@@ -0,0 +1,32 @@
+package com.codecool.bookclub.event.model;
+
+import com.codecool.bookclub.user.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table
+public class EventDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Event event;
+    @JsonIgnoreProperties
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private ParticipantType participantType;
+
+
+}
+
