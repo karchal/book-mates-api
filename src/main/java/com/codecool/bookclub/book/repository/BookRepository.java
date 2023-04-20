07d1f98e4768eb609c26d4a4ef69a1949a7870c0
Aneta Wawer <anwawer1@gmail.com>
2023-04-20 14:41:09 +0200

    method findBookByExternalId implemented

--- src/main/java/com/codecool/bookclub/book/repository/BookRepository.java
+++ src/main/java/com/codecool/bookclub/book/repository/BookRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book findBookById(long id);
+    Book findBookByExternalId(String externalId);
 
     List<Book> findByAuthor(String author);
 
