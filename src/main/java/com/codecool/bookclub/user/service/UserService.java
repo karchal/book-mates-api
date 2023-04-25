51c4abe49199c4711926afc33903afb5af77f401
karchal <k.s.chalupka@gmail.com>
2023-04-25 09:43:59 +0200

    remove all repository fields but UserRepository, create to dto converter

--- src/main/java/com/codecool/bookclub/user/service/UserService.java
+++ src/main/java/com/codecool/bookclub/user/service/UserService.java
@@ -1,19 +1,17 @@
 package com.codecool.bookclub.user.service;
 
 import com.codecool.bookclub.book.dto.BookDetailsDto;
-import com.codecool.bookclub.book.model.BookDetails;
-import com.codecool.bookclub.book.repository.BookDetailsRepository;
+import com.codecool.bookclub.book.service.BookDetailsService;
 import com.codecool.bookclub.event.dto.EventDetailsDto;
-import com.codecool.bookclub.event.repository.EventDetailsRepository;
 import com.codecool.bookclub.event.service.EventService;
 import com.codecool.bookclub.forum.dto.TopicDto;
 import com.codecool.bookclub.forum.model.Comment;
-import com.codecool.bookclub.forum.repository.TopicRepository;
 import com.codecool.bookclub.forum.service.TopicService;
+import com.codecool.bookclub.user.dto.UserDto;
 import com.codecool.bookclub.user.model.User;
 import com.codecool.bookclub.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,47 +21,32 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final BookDetailsRepository bookDetailsRepository;
-
-    private final EventDetailsRepository eventDetailsRepository;
-
-    private final TopicRepository topicRepository;
-
     private final EventService eventService;
-
     private final TopicService topicService;
+    private final BookDetailsService bookDetailsService;
 
-    @Autowired
-    public UserService(UserRepository userRepository, BookDetailsRepository bookDetailsRepository, EventDetailsRepository eventDetailsRepository, TopicRepository topicRepository, EventService eventService, TopicService topicService) {
-        this.userRepository = userRepository;
-        this.bookDetailsRepository = bookDetailsRepository;
-        this.eventDetailsRepository = eventDetailsRepository;
-        this.topicRepository = topicRepository;
-        this.eventService = eventService;
-        this.topicService = topicService;
-    }
 
-    public Optional<User> getUserById(long userId){
+    public Optional<User> getUser(long userId){
         return userRepository.findById(userId);
     }
 
+    public UserDto getUserDto(long userId) {
+        return getUser(userId).map(this::convertToDto).orElse(null);
+    }
+
     public List<BookDetailsDto> getUserBooks(long userId) {
-        return bookDetailsRepository.findAllByUserId(userId)
-                .stream()
-                .map(this::convertToDto)
-                .collect(Collectors.toList());
+        return bookDetailsService.getBookDetailsDtos(userId);
     }
 
     public List<BookDetailsDto> getUserBooks(User user){
         return getUserBooks(user.getId());
     }
+
     public List<EventDetailsDto> getUserEvents(long userId){
-        return eventDetailsRepository.findAllByUserId(userId)
-                .stream()
-                .map(eventService::convertDetailsToDto)
-                .collect(Collectors.toList());
+        return eventService.getUserEventDetailDtos(userId);
     }
 
     public List<EventDetailsDto> getUserEvents(User user){
@@ -71,7 +54,7 @@ public class UserService {
     }
 
     public List<TopicDto> getUserTopics(long userId){
-        return topicRepository.findAllByAuthorId(userId).stream().map(topicService::convertToDto).collect(Collectors.toList());
+        return topicService.getUserTopicDtos(userId);
     }
 
     public List<TopicDto> getUserTopics(User user){
@@ -79,28 +62,36 @@ public class UserService {
     }
 
     public List<Comment> getUserComments(long userId){
-        return getUserById(userId).map(User::getComments).orElse(null);
+        return getUser(userId).map(User::getComments).orElse(null);
     }
 
-
-
     public boolean deleteUserById(long userId){
-        return getUserById(userId).map(user -> {
+        return getUser(userId).map(user -> {
             userRepository.deleteById(userId);
             return true;
         }).orElse(false);
     }
 
-    private BookDetailsDto convertToDto(BookDetails bookDetails){
-        return BookDetailsDto.builder()
-                .bookDetailsId(bookDetails.getId())
-                .externalId(bookDetails.getBook().getExternalId())
-                .title(bookDetails.getBook().getTitle())
-                .author(bookDetails.getBook().getAuthor())
-                .pictureUrl(bookDetails.getBook().getPictureUrl())
-                .shelf(bookDetails.getShelf())
-                .userRating(bookDetails.getUserRating())
-                .build();
+    private UserDto convertToDto(User user) {
+        return (user != null) ? UserDto.builder()
+                .id(user.getId())
+                .nickname(user.getNickname())
+//                .books(getUserBooks(user.getId()))
+                .books(user.getBooksDetails()
+                        .stream()
+                        .map(bookDetailsService::convertToDto)
+                        .collect(Collectors.toList()))
+//                .events(getUserEvents(user.getId()))
+                .events(user.getEvents()
+                        .stream()
+                        .map(eventService::convertDetailsToDto)
+                        .collect(Collectors.toList()))
+//                .topics(getUserTopics(user.getId()))
+                .topics(user.getTopics()
+                        .stream()
+                        .map(topicService::convertToDto)
+                        .collect(Collectors.toList()))
+                .build() : null;
     }
 
 }
