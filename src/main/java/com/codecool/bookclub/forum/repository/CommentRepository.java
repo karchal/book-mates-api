47864f304982a855ef2775468b4afad4592155b8
Aneta Wawer <anwawer1@gmail.com>
2023-04-26 14:37:09 +0200

    implemented findAllByTopicIdOrderByCreationTimeDesc method

--- src/main/java/com/codecool/bookclub/forum/repository/CommentRepository.java
+++ src/main/java/com/codecool/bookclub/forum/repository/CommentRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Topic> findAllByTopic(Topic topic);
+
+    List<Comment> findAllByTopicIdOrderByCreationTimeDesc(long topicId);
 }
