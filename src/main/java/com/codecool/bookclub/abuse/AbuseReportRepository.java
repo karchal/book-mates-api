813330a841a4368ef054307f10563e06e1fc83c4
karchal <k.s.chalupka@gmail.com>
2023-05-23 12:14:40 +0200

    refactor - add condition to check

--- src/main/java/com/codecool/bookclub/abuse/AbuseReportRepository.java
+++ src/main/java/com/codecool/bookclub/abuse/AbuseReportRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AbuseReportRepository extends JpaRepository<AbuseReport, Long> {
 
-    boolean existsByReporterIdAndElementId(Long reporterId, Long elementId);
+    boolean existsByReporterIdAndElementIdAndContentType(Long reporterId, Long elementId, ContentType contentType);
 
 }
