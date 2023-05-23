bcbb5a84150dcb562ef6bb465c628c6b880e77fd
karchal <k.s.chalupka@gmail.com>
2023-05-23 12:19:15 +0200

    combine methods: createReport and reportAbuse from particular classes

--- src/main/java/com/codecool/bookclub/abuse/AbuseReportService.java
+++ src/main/java/com/codecool/bookclub/abuse/AbuseReportService.java
@@ -1,5 +1,7 @@
 package com.codecool.bookclub.abuse;
 
+import com.codecool.bookclub.forum.service.CommentService;
+import com.codecool.bookclub.forum.service.TopicService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,18 +13,35 @@ public class AbuseReportService {
 
     private final AbuseReportRepository abuseReportRepository;
 
+    private final CommentService commentService;
+    private final TopicService topicService;
+
     public ResponseEntity<String> createReport(NewAbuseReportDto newAbuseReportDto, Long userId) {
-        if (abuseReportRepository.existsByReporterIdAndElementId(userId, newAbuseReportDto.getElementId())) {
-            return new ResponseEntity<>("Already reported", HttpStatus.OK);
+        ContentType contentType = newAbuseReportDto.getContentType();
+        Long elementId = newAbuseReportDto.getElementId();
+        if (abuseReportRepository.existsByReporterIdAndElementIdAndContentType(userId, elementId, contentType)) {
+            return new ResponseEntity<>("Już zgłaszałeś problem", HttpStatus.OK);
         }
-        AbuseReport abuseReport = AbuseReport.builder()
-                .elementId(newAbuseReportDto.getElementId())
-                .contentType(newAbuseReportDto.getContentType())
-                .problemType(newAbuseReportDto.getProblemType())
-                .reporterId(userId)
-                .build();
-        abuseReportRepository.save(abuseReport);
-        return new ResponseEntity<>("Already reported", HttpStatus.OK);
+        ResponseEntity<String> responseEntity = reportContent(contentType, elementId);
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            AbuseReport abuseReport = AbuseReport.builder()
+                    .elementId(newAbuseReportDto.getElementId())
+                    .contentType(newAbuseReportDto.getContentType())
+                    .problemType(newAbuseReportDto.getProblemType())
+                    .contentAuthorId(newAbuseReportDto.getContentAuthorId())
+                    .reporterId(userId)
+                    .build();
+            abuseReportRepository.save(abuseReport);
+        }
+        return responseEntity;
+    }
+
+    private ResponseEntity<String> reportContent(ContentType contentType, Long elementId){
+        return switch (contentType) {
+                case COMMENT -> commentService.reportAbuse(elementId);
+                case TOPIC -> topicService.reportAbuse(elementId);
+                case EVENT -> null;
+            };
     }
 
 
