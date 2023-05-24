50df7a9c844da341b27ed11bed0d81910bd1e2d5
Aneta Wawer <anwawer1@gmail.com>
2023-05-24 14:42:47 +0200

    changed resignFromEvent http method to PUT

--- src/main/java/com/codecool/bookclub/event/controller/EventController.java
+++ src/main/java/com/codecool/bookclub/event/controller/EventController.java
@@ -85,7 +85,7 @@ public class EventController {
         return new ResponseEntity<>(events, HttpStatus.OK).getBody();
     }
 
-    @DeleteMapping("events/{event_id}/resign")
+    @PutMapping("events/{event_id}/resign")
     public void resignFromEvent(@PathVariable("event_id") long eventId, @AuthenticationPrincipal Long userId){
         eventService.resignFromEvent(eventId, userId);
     }
